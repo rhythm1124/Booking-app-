bbf53b5e557f4d386285a52ba2518f136d4e8c34
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-08-29 01:27:50 +0530

    main form - page creation

---
+++ bookingapp/src/pages/main_form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import '../styles/main_form.css';
+
+const Form = () => {
+    return (
+        <div>
+            <h1>main form details</h1>
+        </div>
+    )
+}
+
+export default Form;
