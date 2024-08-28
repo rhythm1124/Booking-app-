64026d343fbda7cf61bb5970268c6e5e3541d088
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-08-29 01:56:33 +0530

    Update main_form.js

--- bookingapp/src/pages/main_form.js
+++ bookingapp/src/pages/main_form.js
@@ -1,10 +1,24 @@
 import React from 'react';
-// import '../styles/main_form.css';
+import '../styles/main_form.css';
 
 const Form = () => {
     return (
         <div>
+            <div class="side-container">
+                <ul>
+                    <li>Details</li>
+                    <li>Event</li>
+                    <li>Confirm</li>
+                </ul>
+            </div>
             <h1>main form details</h1>
+            <ul class="color-theme">
+                <li id="a">cool blue</li>
+                <li id="b">light greyish</li>
+                <li id="c">macadamia</li>
+                <li id="d">redwood</li>
+                <li id="e">burnt under</li>
+            </ul>
         </div>
     )
 }
