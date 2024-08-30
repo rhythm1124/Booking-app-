e3f105ea9a337eb04f01171bc0f4d89315bd31bd
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-08-30 18:28:07 +0530

    side container update

--- bookingapp/src/components/sideconatiner.js
+++ bookingapp/src/components/sideconatiner.js
@@ -1,25 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/sidecontainer.css';
 
 const SideContainer = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
     return (
-        <div>
-            <div className="hamburger" onClick={toggleMenu}>
-                &#9776; {/* Unicode for the hamburger icon */}
-            </div>
-            <div className={`side-container ${isOpen ? 'show' : ''}`}>
-                <ul>
-                    <li><a href="#details">Details</a></li>
-                    <li><a href="#event">Event</a></li>
-                    <li><a href="#confirm">Confirm</a></li>
-                </ul>
-            </div>
+        <div className="side-container">
+            <ul>
+                <li><a href="#details">Details</a></li>
+                <li><a href="#event">Event</a></li>
+                <li><a href="#confirm">Confirm</a></li>
+            </ul>
         </div>
     );
 }
