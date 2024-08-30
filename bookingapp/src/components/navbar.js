2a9b348f2907a359a2441e26b9123338b9d3d922
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-08-30 18:27:59 +0530

    update navbar

--- bookingapp/src/components/navbar.js
+++ bookingapp/src/components/navbar.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/navbar.css';
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav className="navbar"> {/* Corrected the typo from "classNmae" to "className" */}
+        <nav className="navbar">
             <div className="navbar-title">Title</div>
             <div className="navbar-user">User</div>
+            <div className="navbar-hamburger" onClick={toggleMenu}>
+                &#9776; {/* Unicode for the hamburger icon */}
+            </div>
+            {/* Menu dropdown */}
+            <div className={`side-menu ${isOpen ? 'show' : ''}`}>
+                <ul>
+                    <li><a href="#details">Details</a></li>
+                    <li><a href="#event">Event</a></li>
+                    <li><a href="#confirm">Confirm</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
