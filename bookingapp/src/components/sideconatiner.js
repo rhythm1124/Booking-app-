ed6cf61fe69c27f81afd8e288a2a2514591ddc39
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-09-04 22:28:35 +0530

    update side container

--- bookingapp/src/components/sideconatiner.js
+++ bookingapp/src/components/sideconatiner.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
 import '../styles/sidecontainer.css';
 
 const SideContainer = () => {
     return (
         <div className="side-container">
             <ul>
-                <li><a href="#details">Details</a></li>
-                <li><a href="#event">Event</a></li>
-                <li><a href="#confirm">Confirm</a></li>
+                <li><Link to="/">1</Link></li> {/* Main page */}
+                <li><Link to="/form">2</Link></li> {/* Form page */}
+                <li><Link to="/confirmation">3</Link></li> {/* Confirmation page */}
             </ul>
         </div>
     );
