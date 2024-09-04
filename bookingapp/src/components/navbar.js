055b061f1a07f7087b1f782290d981ab75d839a5
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-09-05 01:04:35 +0530

    Update navbar.js

--- bookingapp/src/components/navbar.js
+++ bookingapp/src/components/navbar.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import '../styles/navbar.css';
 
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
+    // Map current path to page title
+    const getPageTitle = () => {
+        switch (location.pathname) {
+            case '/':
+                return 'Select Movie';
+            case '/form':
+                return 'Add Details';
+            case '/confirmation':
+                return 'Confirm';
+            default:
+                return 'Title';
+        }
+    };
+
     return (
         <nav className="navbar">
-            <div className="navbar-title">Title</div>
+            <div className="navbar-title">{getPageTitle()}</div>
             <div className="navbar-user">User</div>
             <div className="navbar-hamburger" onClick={toggleMenu}>
                 &#9776; {/* Unicode for the hamburger icon */}
