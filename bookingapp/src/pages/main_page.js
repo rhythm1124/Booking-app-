9eb98adc93e5779b37822f17d97b7e91b9e970f6
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-09-04 23:18:09 +0530

    Create main_page.js

---
+++ bookingapp/src/pages/main_page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div className="main-content">
+            <h1>Select a Movie</h1>
+            {/* Movie selection content goes here */}
+        </div>
+    );
+}
+
+export default MainPage;
