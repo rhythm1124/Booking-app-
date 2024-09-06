d386cf27ca9d10b68bd03b646567cf37e4cc64ee
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-09-06 14:41:14 +0530

    adding the imgs

--- bookingapp/src/pages/main_page.js
+++ bookingapp/src/pages/main_page.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/main_page.css';
+import poster1 from '../assets/url_to_poster_1.jpg';
+import poster2 from '../assets/url_to_poster_2.jpg';
+import poster3 from '../assets/url_to_poster_3.jpg';
+import poster4 from '../assets/url_to_poster_4.jpg';
+import poster5 from '../assets/url_to_poster_5.jpg';
+import poster6 from '../assets/url_to_poster_6.jpg';
+import poster7 from '../assets/url_to_poster_7.jpg';
+import poster8 from '../assets/url_to_poster_8.jpg';
+import poster9 from '../assets/url_to_poster_9.jpg';
 
 const movies = [
-    { name: 'Movie 1', poster: 'url_to_poster_1.jpg' },
-    { name: 'Movie 2', poster: 'url_to_poster_2.jpg' },
-    { name: 'Movie 3', poster: 'url_to_poster_3.jpg' },
-    { name: 'Movie 4', poster: 'url_to_poster_4.jpg' },
-    { name: 'Movie 5', poster: 'url_to_poster_5.jpg' },
-    { name: 'Movie 6', poster: 'url_to_poster_6.jpg' },
-    { name: 'Movie 7', poster: 'url_to_poster_7.jpg' },
-    { name: 'Movie 8', poster: 'url_to_poster_8.jpg' },
-    { name: 'Movie 9', poster: 'url_to_poster_9.jpg' }
+    { name: 'Avengers', poster: poster1 },
+    { name: 'Star Wars', poster: poster2 },
+    { name: 'Shrek', poster: poster3 },
+    { name: 'Stree 2', poster: poster4 },
+    { name: '83', poster: poster5 },
+    { name: 'Deadpool X Wolverine', poster: poster6 },
+    { name: 'Jab We Met', poster: poster7 },
+    { name: 'Yeh Jaavani Hai Deewani', poster: poster8 },
+    { name: 'Tamasha', poster: poster9 }
 ];
 
 const MainPage = () => {
